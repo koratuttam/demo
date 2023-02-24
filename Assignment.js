bdf2706e132bc1ae468dfabfaaeb393d6553aea0
korat uttam <koratuttam456@gmail.com>
2023-02-23 19:14:06 -0500

    third

--- Assignment.js
+++ Assignment.js
@@ -4,7 +4,7 @@ const port = 8080 || process.env.port
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('otherfile.html', function(error, data){
+    fs.readFile('index.html', function(error, data){
         if(error){
             res.writeHead(404)
             res.write('<h1 style="color: red">The page you requested is not available</h1>')
